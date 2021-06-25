d8c77c3bb02f3902a0fe9a7f4d6696c305dc2ec2
Greg Dumas <gregrdumas@outlook.com>
2021-06-25 15:38:05 -0400

    Refactor main() to use XenaPythonLib

--- XenaPktSend.py
+++ XenaPktSend.py
@@ -54,7 +54,8 @@ def main(args):
     stream_logger.setFormatter(logging.Formatter(
         '[%(levelname)-5s]  %(asctime)s : (%(name)s) - %(message)s'))
     _LOGGER.addHandler(stream_logger)
-    xena_socket = XenaSocketDriver(args.chassis)
+    xena_socket = XenaSocket(args.chassis)
+    xena_socket.connect()
     time.sleep(1)
 
     # create the manager session
@@ -62,40 +63,67 @@ def main(args):
     time.sleep(1)
     try:
         # add port 0 and configure
-        port0 = xm.add_module_port(args.module, args.ports[0])
-        port0.reserve_port()
-        port0.clear_stats()
+        port0 = xm.add_port(args.module, args.ports[0])
+        port0.reserve()
+        port0.clear_all_rx_stats()
+        port0.clear_all_tx_stats()
         # add port 1 and configure
-        port1 = xm.add_module_port(args.module, args.ports[1])
-        port1.reserve_port()
-        port1.clear_stats()
+        port1 = xm.add_port(args.module, args.ports[1])
+        port1.reserve()
+        port1.clear_all_rx_stats()
+        port1.clear_all_tx_stats()
     except Exception as e:
         print('An exception occurred while attempting to add and configure ports')
         print(e)
     try:
         # add a single stream and configure
-        s1_p0 = port0.add_stream()
+        s1_p0 = port0.add_stream(0)
         s1_p0.set_packet_limit(args.duration * args.pps)
-        s1_p0.set_on()
-        s1_p0.set_packet_per_second(args.pps)
+        s1_p0.set_stream_on()
+        s1_p0.set_rate_pps(args.pps)
         s1_p0.set_packet_header(pkthdr1)
-        s1_p0.set_packet_length('fixed', args.pkt_size, 1518)
-        s1_p0.set_packet_payload('incrementing', '0x00')
-        s1_p0.set_header_protocol('ETHERNET IP')
-        s1_p0.set_payload_id(1)
+        s1_p0.set_packet_length_fixed(args.pkt_size, 1518)
+        s1_p0.set_packet_payload_incrementing('0x00')
+        s1_p0.set_packet_protocol('ETHERNET', 'IP')
+        s1_p0.set_test_payload_id(1)
+        
+        # enable multistream
         if args.number_streams:
-            s1_p0.enable_multistream(args.number_streams, 'L3')
-        port0.traffic_on()
+            m1_s1_p0 = s1_p0.add_modifier()
+            m1_s1_p0.set_modifier(32, 0xffff0000, 'inc', 1)
+            m1_s1_p0.set_modifier_range(0, 1, args.number_streams)
+        
+        # begin network traffic
+        port0.start_traffic()
         time.sleep(args.duration + 2)
-        port0.traffic_off()
-        stat = port1.get_rx_stats()
-        print(stat.data)
+        port0.stop_traffic()
+        
+        # retrieve traffic statistics
+        port1.grab_all_rx_stats()
+        grab_stats = port1.dump_all_rx_stats()
+        
+        # reorganize stats dict
+        stats = {}
+        for timestamp, stat in grab_stats.items():
+            for key in stat:
+                stats[key] = stat[key]
+
+        # packets sent
         pkt_sent = args.duration * args.pps
         print('Packets sent: {}'.format(pkt_sent))
-        pkt_rec = stat.data['pr_total']['packets']
-        pkt_latency_avg = stat.data['pr_tpldlatency']['1']['avg']
+        
+        # packets received
+        pr_total = stats.get('pr_total')
+        pkt_rec = pr_total.get('packets')
+        
+        # latency
+        pr_tpldlatency = stats.get('pr_tpldlatency')
+        tmp = pr_tpldlatency.get('1')
+        pkt_latency_avg = tmp.get('avg')
         print('Packet latency: {} ns'.format(pkt_latency_avg))
-        pkt_lost = pkt_sent - pkt_rec
+        
+        # packets lost
+        pkt_lost = pkt_sent - pkt_rec # sometimes returns negative loss, TODO: find out why
         print('Packets received: {}'.format(pkt_rec))
         print('Packets lost: {}'.format(pkt_lost))
     except Exception as e:
@@ -104,7 +132,6 @@ def main(args):
     finally:
         # disconnect from Xena
         print('Disconnecting from Xena chassis...')
-        xm.disconnect()
         del xm
         del xena_socket
         print('Connection severed')
